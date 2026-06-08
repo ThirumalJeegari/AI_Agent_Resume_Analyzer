6b8eb36ee4871123539bc7a0d459d89b4346ba25
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-06-09 01:33:09 +0530

    Full Updated code

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from PyPDF2 import PdfReader
 
-SERVER_URL = "http://127.0.0.1:8000"
+SERVER_URL = "https://ai-agent-resume-analyzer.onrender.com"
 
 st.title("AI Resume Analyzer Agent")
 
